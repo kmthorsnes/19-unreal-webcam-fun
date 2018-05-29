8777ed9241367304c87c013cafec5b872c1c1ba0
kalle <kmthorsnes@gmail.com>
2018-05-29 11:55:15 +0200

    wip

--- scripts.js
+++ scripts.js
@@ -8,8 +8,38 @@ function getVideo(){
     navigator.mediaDevices.getUserMedia({ video: true, audio:false })
         .then(localMediaStream => {
             console.log(localMediaStream);
-            video.src = localMediaStream;
-        });
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error('Neiiii, du må jo la meg bruke ditt webcamera!, err');
+        })
+}
+
+function paintToCanavas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        }, 16);
+}
+
+function takePhoto(){
+    snap.currentTime = 0; 
+    snap.play();
+
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Kjekkas"; />`;
+    strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
+
+video.addEventListener('canplay', )
